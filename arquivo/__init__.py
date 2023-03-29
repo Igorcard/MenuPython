0b8794bcb336dbd94ebc2d23a41975630b1dcdc4
Igor Cardoso Françolin <Ishvar@Igor-PC.(none)>
2023-03-29 15:22:01 -0300

    Adicionando arquivo

---
+++ arquivo/__init__.py
@@ -0,0 +1,11 @@
+import setuptools
+
+def arquivoExiste(nome):
+    try:
+        a = open(nome, 'rt')
+        a.close()
+    except FileNotFoundError:
+        return False
+    else:
+        return True
+    
